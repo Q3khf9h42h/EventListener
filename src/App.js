008b8b6b5ece0a64e31915e0b81cc781d93b27d4
harshita sethiya <harshita.sethiya252@gmail.com>
2023-07-27 09:42:41 +0530

    demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
-
+import ExpenseItem from './components/ExpenseItem';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const expenses = [
+    {
+      id:'e1',
+      title:'Toilet Paper',
+     amount :94.12,
+    date: new Date(2019, 7 ,27),
+    locationOfExpenditure: 'Store A',
+  },
+    { id: 'e2', title:'New TV',amount:298.45,date: new Date(2023,7,26),locationOfExpenditure: 'Store C',},
+    {
+      id:'e3',
+      title:'car insurance',
+     amount :95.12,
+    date: new Date(2020, 7 ,27),
+    locationOfExpenditure: 'Store B',
+  },
+  
+    
+    {
+      id:'e4',
+      title:'car engine',
+     amount :97.12,
+    date: new Date(2022, 7 ,27),
+    locationOfExpenditure: 'Store D',
+  },
+    
+  ];
+  return(
+    <div>
+    <h2>Let's get started! </h2>
+    <ExpenseItem 
+    title={expenses[0].title} 
+    amount={expenses[0].amount} 
+    date={expenses[0].date}
+    locationOfExpenditure={expenses[0].locationOfExpenditure}
+    >
+ </ExpenseItem>
+    <ExpenseItem
+    title={expenses[1].title} 
+    amount={expenses[1].amount} 
+    date={expenses[1].date}
+    locationOfExpenditure={expenses[1].locationOfExpenditure}>
+    </ExpenseItem>
+    <ExpenseItem
+    title={expenses[2].title} 
+    amount={expenses[2].amount} 
+    date={expenses[2].date}
+    locationOfExpenditure={expenses[2].locationOfExpenditure}>
+    </ExpenseItem>
+    <ExpenseItem
+    title={expenses[3].title} 
+    amount={expenses[3].amount} 
+    date={expenses[3].date}
+    locationOfExpenditure={expenses[3].locationOfExpenditure}>
+    </ExpenseItem>
+    
+    
+
+  
+  
+    
     </div>
   );
 }
